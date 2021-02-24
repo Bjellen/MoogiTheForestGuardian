881068060d56af7f9c240735e084ffec825b2245
Isabella S.  Skorpen <isabella.sorensen@live.no>
2021-02-24 14:41:15 +0100

    Movement = Done

--- Assets/Scripts/MovementScripts/PlayerController.cs
+++ Assets/Scripts/MovementScripts/PlayerController.cs
@@ -11,6 +11,10 @@ public class PlayerController : MonoBehaviour
 
     [Header("the bod")]
     private Rigidbody2D rb2D;
+    private bool facingRight;
+
+    [Header("GroundCheck")]
+    public LayerMask whatIsGround;
 
     [Header("Input Checks")]
     public float x;
@@ -26,6 +30,45 @@ public class PlayerController : MonoBehaviour
         x = Input.GetAxisRaw("Horizontal");
         //y = Input.GetAxisRaw("Vertical");
 
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        { Jump(); }
+
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+
+        if(facingRight == false && x < 0) { Flip(); }
+        else if(facingRight == true && x > 0 ) { Flip(); }
+    }
+
+    void Movement()
+    {
+        rb2D.velocity = new Vector2(x * moveSpeed, rb2D.velocity.y);
+    }
+
+    void Jump()
+    {
+        rb2D.velocity = new Vector2(rb2D.velocity.x, jumpForce);
+    }
+
+    bool IsGrounded()
+    {
+        var _position = transform.position;
+        var _direction = -Vector2.up;
+        float _distance = 1.5f;
+
+        var hit = Physics2D.Raycast(_position, _direction, _distance, whatIsGround);
+        Debug.DrawRay(_position, _direction, Color.green);
+
+        return hit.collider != null;
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
     }
 
 }
