a54dd01e23d7b8e52a774c7cd10db4cd09ca2db1
Isabella S.  Skorpen <isabella.sorensen@live.no>
2021-02-26 14:10:04 +0100

    Enemy progress
    gikk litt treigt på slutten så gjør hva jeg ikke fikk gjort på mandag c:

---
+++ Assets/Scripts/Enemies/WalkingEnemies/Enemy_Walk.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Enemy_Walk : MonoBehaviour
+{
+    private Rigidbody2D enemyRb;
+
+    [Header("Movement")]
+    public float moveSpeed;
+
+    [Header("Movement Vectors")]
+    public Vector2[] moveDirections;
+    public int directionIndex;
+    [Tooltip ("Be the amount of movedirections the MoveDirection array says")]
+    public int MaxDirectionIndex;
+
+    private bool changeDir;
+
+    private void Start()
+    {
+        enemyRb = GetComponent<Rigidbody2D>();
+
+        directionIndex = 0;
+
+        InvokeRepeating("SwitchDirection", 2, 2);
+    }
+
+    private void Update()
+    {
+        if (changeDir == true)
+        {
+            ChangeDirection();
+
+            changeDir = false;
+        }
+
+        if (directionIndex == 2) { Flip(); }
+
+        if (directionIndex >= MaxDirectionIndex)
+        {
+            FlipBack();
+            directionIndex = 0;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        enemyRb.velocity = moveDirections[directionIndex] * moveSpeed;
+    }
+
+    #region ChangeDirection functions
+    void ChangeDirection()
+    {
+        directionIndex++;
+    }
+
+    void SwitchDirection()
+    {
+        if(changeDir) { changeDir = false; }
+        else { changeDir = true; }
+    }
+    #endregion
+
+    #region Flip functions
+    void Flip()
+    {
+        transform.rotation = Quaternion.Euler(0, 180, 0);
+    }
+    
+    void FlipBack()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+    #endregion
+}
