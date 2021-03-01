c92de6a217f5b8368607c1682b8863967cc75ba4
Isabella S.  Skorpen <isabella.sorensen@live.no>
2021-03-01 14:15:23 +0100

    Minor progress
    Treg mandag D:

---
+++ Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [Header("Player Healths")]
+    public int playerHealth = 3;
+
+    [Header("What hurts player")]
+    public string dangerTag;
+
+    [Header("What scene to load when;")]
+    public string deathScene;
+
+    [Header("Spawnpoints")]
+    public Transform spawnPoint;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(dangerTag) == true)
+        {
+            TakeDamage();
+        }
+    }
+    
+    void TakeDamage()
+    {
+        playerHealth -= 1;
+        transform.position = spawnPoint.transform.position;
+        Debug.Log("I have taken damage");
+
+        if(playerHealth <= 0)
+        {
+            Debug.Log("I have died");
+            //Die();
+        }
+    }
+
+    void Die()
+    {
+        SceneManager.LoadScene(deathScene);
+    }
+}
