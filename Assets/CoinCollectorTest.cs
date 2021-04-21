5db70648eb43af5c1c7080fdaaab8862ae4c177e
Mia <miabergmann@hotmail.com>
2021-04-21 19:17:28 +0200

    testscene colletables

---
+++ Assets/CoinCollectorTest.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinCollectorTest : MonoBehaviour
+{
+    private float coin = 0;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       if(other.transform.tag == "Coin")
+        {
+            coin++;
+            Destroy(other.gameObject);
+        }
+    }
+}
