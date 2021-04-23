4ce5a96fa2b9ba9e77b9e869653d95f27bf91e21
Mia <miabergmann@hotmail.com>
2021-04-23 23:39:47 +0200

    ReturnToMainMenu

--- Assets/Scripts/End.cs
+++ Assets/Scripts/End.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class End : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string MainMenu;
+    public void mainMenu()
     {
-        
+        SceneManager.LoadScene(0);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
