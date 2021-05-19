0276c6bde6601835da0bcb236e60bf9472092881
Mia <miabergmann@hotmail.com>
2021-05-19 15:47:15 +0200

    Animation Jumping Mushrooms

--- Assets/Scripts/Environment/Bouncy_Mushroom.cs
+++ Assets/Scripts/Environment/Bouncy_Mushroom.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bouncy_Mushroom : MonoBehaviour
 {
     public float jumpForce;
+    public Animator animator;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -12,6 +13,7 @@ public class Bouncy_Mushroom : MonoBehaviour
         {
             Rigidbody2D rb2D = collision.gameObject.GetComponent<Rigidbody2D>();
             rb2D.velocity = new Vector2(rb2D.velocity.x, jumpForce);
+            animator.Play("Scared");
         }
     }
 }
